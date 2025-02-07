b1956d5bfccfb34952653b42e707dcf5f9c58524
sanjitkumar33 <kumarsan5233@gmail.com>
2025-02-07 17:26:52 +0530

    code update

--- calc/calc.js
+++ calc/calc.js
@@ -1,39 +1,33 @@
-//addition part of javascript
-function add(){
-    let a = document.getElementById('first').value;
-    let b = document.getElementById('second').value;
-    let out = Number(a)+Number(b)
-    document.getElementsByClassName('output')[0].innerText=`sum of number is ${out}`
-}
-//substraction part of javascript
-function sub(){
-    let a = document.getElementById('first').value;
-    let b = document.getElementById('second').value;
-    let out = Number(a)-Number(b)
-    document.getElementsByClassName('output')[0].innerText=`sub of number is ${out}`
-}
-//muultiplication part of javascript
-function mul(){
-    let a = document.getElementById('first').value;
-    let b = document.getElementById('second').value;
-    let out = Number(a)*Number(b)
-    document.getElementsByClassName('output')[0].innerText=`mul of number is ${out}`
-}
-//division part of javascript
-function div(){
-    let a = document.getElementById('first').value;
-    let b = document.getElementById('second').value;
-    if (Number(b) ===0){
-        document.getElementsByClassName('output')[0].innerText = "cannot divide by zero!";
-    }else{
-        let out = Number(a)/Number(b);
-        document.getElementsByClassName('output')[0].innerText = `Division of numbers is ${out}`;
+// Select the result input field
+const result = document.getElementById("result");
 
+// Function to clear the input
+function clearFields() {
+    result.value = "";
+}
+
+// Function to append clicked button values to the input
+function appendValue(value) {
+    result.value += value;
+}
+
+// Function to evaluate the expression
+function calculate() {
+    try {
+        result.value = eval(result.value); // Evaluate the expression
+    } catch (error) {
+        result.value = "Error"; // Handle invalid expressions
     }
 }
-//clear fields parts of javascript
-function clearFields(){
-    document.getElementById('first').value = '';
-    document.getElementById('second').value = '';
-    document.getElementsByClassName('output')[0].innerText = '';
-}
+
+// Attach event listeners to buttons dynamically
+document.querySelectorAll(".number, .operator, .decimal").forEach(button => {
+    button.addEventListener("click", () => appendValue(button.textContent));
+});
+
+// Attach event listener to the equals button
+document.querySelector(".equal").addEventListener("click", calculate);
+
+// Attach event listener to the clear button
+document.querySelector(".clear").addEventListener("click", clearFields);
+
