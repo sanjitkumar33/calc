2b7904a75e9d535d5a00d3131fca04d981368fb6
sanjitkumar33 <kumarsan5233@gmail.com>
2025-03-03 13:41:12 +0530

    add code

--- stringopt.js
+++ stringopt.js
@@ -5,3 +5,15 @@ var b = "john"
 
 var a = "Sanjit"
 var b = "sanjit"
+
+var city = "amSterdam"
+city.charAt(0).toUpperCase()
+
+var uname = "   Ankita "
+
+var uname = "    Ankita"
+undefined
+uname.length
+10
+uname.trim()
+'Ankita'
