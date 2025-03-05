89a8cd950ccb667ca008183712ff18b0a5372905
sanjitkumar33 <kumarsan5233@gmail.com>
2025-03-05 21:27:01 +0530

    add code

---
+++ coupons/coupans.js
@@ -0,0 +1,10 @@
+function loadCoupan(){
+    document.getElementById('coupan').style.visibility = 'visible';
+    document.getElementById('main').style.opacity = '0.7';
+
+}
+function closeCoupan(){
+    document.getElementById('coupan').style.visibility = 'hidden';
+    document.getElementById('main').style.opacity = '1';
+
+}
