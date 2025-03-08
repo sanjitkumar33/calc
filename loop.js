a4edb10150563777cbd1c26ffce4334f6c02c5dc
sanjitkumar33 <kumarsan5233@gmail.com>
2025-03-08 17:09:03 +0530

    add code

---
+++ loop.js
@@ -0,0 +1,18 @@
+var city = ["Helsinki","London","Amercia","haweia","Boston"]
+for(i=0;i<city.length;i++)
+{
+    console.log(city[i])
+}
+
+var i = 10;
+while(i<6){
+    console.log(i);
+    i++
+}
+
+var i = 10;
+do{
+    console.log(i);
+    i++
+}
+while(i<5)
